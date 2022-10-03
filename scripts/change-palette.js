21a077ae952d84b01fd3b0e45d2efadf29420d3b
Tidykiwi <rosskeithbaker@gmail.com>
2022-10-03 17:12:17 +1300

    Updated structure

--- scripts/change-palette.js
+++ scripts/change-palette.js
@@ -4,10 +4,13 @@ const palette3 = document.getElementById('palette-3')
 const h1s = document.getElementsByTagName('h1')
 const ps = document.getElementsByTagName('p')
 const main = document.getElementById('main')
-const homeContent = document.getElementById('home-content')
+const contents = document.getElementsByClassName('content-container')
 const projectCards = document.getElementsByClassName('card')
-const gridItemOne = document.getElementById('intro-grid-item-1')
-const gridItemTwo = document.getElementById('intro-grid-item-2')
+const flexItemOne = document.getElementById('intro-flex-item-1')
+const flexItemTwo = document.getElementById('intro-flex-item-2')
+const modalContent = document.getElementById('modal-content')
+const modalContainer = document.getElementById('container-modal')
+const buttons = document.getElementsByClassName('button')
 
 document.getElementById("palette-1").addEventListener("click", changePaletteOne);
 
@@ -22,16 +25,26 @@ function changePaletteOne() {
     main.style.backgroundColor = '#5c6b73'
     main.style.border = 'solid 10px #253237'
 
-    homeContent.style.border = 'solid 10px #9db4c0'
+    for (const content of contents) {
+        content.style.border = 'solid 10px #9db4c0'
+        content.style.backgroundColor = '#e0fbfc'
+    }
 
     for (const projectCard of projectCards) {
         projectCard.style.backgroundColor = '#c2dfe3'
     } 
+    
+    modalContent.style.backgroundColor = '#e0fbfc'
+    modalContent.style.border = 'solid 10px #c2dfe3'
+    modalContainer.style.backgroundColor = 'rgba(92, 107, 115, 0.9)'
 
-    homeContent.style.backgroundColor = '#e0fbfc'
+    flexItemOne.style.backgroundColor = '#c2dfe3'
+    flexItemTwo.style.backgroundColor = '#9db4c0'
 
-    gridItemOne.style.backgroundColor = '#c2dfe3'
-    gridItemTwo.style.backgroundColor = '#9db4c0'
+    for (const button of buttons) {
+        button.style.backgroundColor = '#5c6b73'
+    }
+    
 }
 
 function changePaletteTwo() {
@@ -45,16 +58,26 @@ function changePaletteTwo() {
     main.style.backgroundColor = '#40798c'
     main.style.border = 'solid 10px #1f363d'
 
-    homeContent.style.border = 'solid 10px #70a9a1'
+    for (const content of contents) {
+        content.style.border = 'solid 10px #70a9a1'
+        content.style.backgroundColor = '#cfe0c3'
+    }   
 
     for (const projectCard of projectCards) {
         projectCard.style.backgroundColor = '#9ec1a3'
     } 
 
-    homeContent.style.backgroundColor = '#cfe0c3'
+    modalContent.style.backgroundColor = '#cfe0c3'
+    modalContent.style.border = 'solid 10px #9ec1a3'
+    modalContainer.style.backgroundColor = 'rgba(64, 121, 140, 0.8)'
 
-    gridItemOne.style.backgroundColor = '#9ec1a3'
-    gridItemTwo.style.backgroundColor = '#70a9a1'
+    flexItemOne.style.backgroundColor = '#9ec1a3'
+    flexItemTwo.style.backgroundColor = '#70a9a1'
+
+    for (const button of buttons) {
+        button.style.backgroundColor = '#40798c'
+    }
+    
 }
 
 function changePaletteThree() {
@@ -68,14 +91,23 @@ function changePaletteThree() {
     main.style.backgroundColor = '#4a4e69'
     main.style.border = 'solid 10px #22223b'
 
-    homeContent.style.border = 'solid 10px #9a8c98'
+    for (const content of contents) {
+        content.style.border = 'solid 10px #9a8c98'
+        content.style.backgroundColor = '#f2e9e4'    
+    }
 
     for (const projectCard of projectCards) {
         projectCard.style.backgroundColor = '#c9ada7'
     } 
 
-    homeContent.style.backgroundColor = '#f2e9e4'
+    modalContent.style.backgroundColor = '#f2e9e4'
+    modalContent.style.border = 'solid 10px #c9ada7'
+    modalContainer.style.backgroundColor = 'rgba(74, 78, 105, 0.8)'
 
-    gridItemOne.style.backgroundColor = '#c9ada7'
-    gridItemTwo.style.backgroundColor = '#9a8c98'
+    flexItemOne.style.backgroundColor = '#c9ada7'
+    flexItemTwo.style.backgroundColor = '#9a8c98'
+
+    for (const button of buttons) {
+        button.style.backgroundColor = '#4a4e69'
+    }
 }
