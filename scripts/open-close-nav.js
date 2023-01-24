f99309850a15eb15f87ded3dcb1236cbe2ef9197
Tidykiwi <rosskeithbaker@gmail.com>
2023-01-24 22:11:03 +1300

    Updated navigation

--- scripts/open-close-nav.js
+++ scripts/open-close-nav.js
@@ -1,44 +1,42 @@
+let width = window.innerWidth
+
 function openNav() {
 
-    let width = window.innerWidth
-    console.log(width)
+  let width = window.innerWidth
 
-    if (width > 600) {
+  if (width > 600) {
 
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginleft = "250px";
-      document.getElementById("mySidenav").style.height = "100%";
-
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.height = "100%";
+    document.getElementById("mySidenav").style.paddingTop = "60px";
+    document.getElementById("main").style.marginLeft = "250px";
     
-    } else if (width < 600) {
+  } else if (width < 600) {
 
-      document.getElementById("mySidenav").style.width = "100%";
-      document.getElementById("mySidenav").style.height = "250px";
-      document.getElementById("main").style.marginTop = "250px";
-
+    document.getElementById("mySidenav").style.width = "100vw";
+    document.getElementById("mySidenav").style.height = "250px";
+    document.getElementById("mySidenav").style.paddingTop = "60px";
+    document.getElementById("main").style.marginTop = "250px";
      
-    }
-  
   }
+}
   
-  function closeNav() {
+function closeNav() {
 
-    let width = window.innerWidth
-    console.log(width)
+  let width = window.innerWidth
 
-    if (width > 600) {
+  if (width > 600) {
 
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginleft = "0";
-      document.getElementById("main").style.marginTop = "0";
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.height = "100%";
+    document.getElementById("main").style.marginLeft = "0";
     
-    } else if (width < 600) {
+  } else if (width < 600) {
 
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("mySidenav").style.height = "100%";
-      document.getElementById("main").style.marginTop = "0";
-
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.height = "100%";
+    document.getElementById("mySidenav").style.paddingTop = "0";
+    document.getElementById("main").style.marginTop = "0";
      
-    }
-
   }
+}
