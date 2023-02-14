ecfa2961e425f4dbe0d09fd69260dbd94e55dc03
Tidykiwi <rosskeithbaker@gmail.com>
2023-02-14 22:12:47 +1300

    Updated Client Cards

--- scripts/open-close-nav.js
+++ scripts/open-close-nav.js
@@ -1,6 +1,11 @@
 let width = window.innerWidth
 
+let navOpen = false;
+
 function openNav() {
+
+  navOpen = true;
+  console.log("TRUE")
 
   let width = window.innerWidth
 
@@ -20,8 +25,11 @@ function openNav() {
      
   }
 }
-  
+
 function closeNav() {
+
+  navOpen = false;
+  console.log("FALSE")
 
   let width = window.innerWidth
 
@@ -39,4 +47,34 @@ function closeNav() {
     document.getElementById("main").style.marginTop = "0";
      
   }
+}
+
+function navWindowResize() {
+
+  if (navOpen) {
+
+    let width = window.innerWidth
+
+    if (width > 600) {
+  
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("mySidenav").style.height = "100%";
+      document.getElementById("mySidenav").style.paddingTop = "60px";
+      document.getElementById("main").style.marginLeft = "242px";
+      document.getElementById("main").style.marginTop = "0";
+      
+    } else if (width < 600) {
+  
+      document.getElementById("mySidenav").style.width = "100vw";
+      document.getElementById("mySidenav").style.height = "250px";
+      document.getElementById("mySidenav").style.paddingTop = "60px";
+      document.getElementById("main").style.marginTop = "250px";
+      document.getElementById("main").style.marginLeft = "0";
+       
+    }
+
+    console.log("RESIZE")
+
+  } 
+
 }
